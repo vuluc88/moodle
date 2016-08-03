7e9b63cb06f0c7a5d6a4287dc79044e594f93081
Luc Vu Huy <lucvu@HarveyNash.vn.local>
2016-08-03 16:09:02 +0700

    Revert "Block tutorial"
    This reverts commit 76bc094af5d224407733697b2e69fe7e460e3a80.

--- block_tutorial/helloblock/version.php
+++
@@ -1,4 +0,0 @@
-<?php
-$plugin->component = 'block_helloblock';
-$plugin->version = 2016080110;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
