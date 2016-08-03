592b99198079527122c94187d9cab164ccfb5eb1
Luc Vu Huy <lucvu@HarveyNash.vn.local>
2016-08-03 16:10:29 +0700

    block tutorial

---
+++ block_tutorial/helloblock/version.php
@@ -0,0 +1,4 @@
+<?php
+$plugin->component = 'block_helloblock';
+$plugin->version = 2016080110;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
